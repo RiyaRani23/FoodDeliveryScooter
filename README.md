9b0987affc3e4a8cda3a70315abc017ffc651588
RiyaRani23 <riyarani1404@gmail.com>
2025-12-26 17:54:02 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FoodDeliveryScooter
+
+ 
+`Heading`
+  - logo
+  - Nav Items
+`Body`
+  - search
+  - restaurant list
+    - Restaurant Card
+      - image
+        - name of restaurant,Star Rating,Cuisine,Delivery time
+`Footer`
+  -Copyright
+  - links   
+  - address
+  - contact
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                  <img className="logo" src="https://png.pngtree.com/template/20200610/ourmid/pngtree-food-delivery-logo-design-image_381319.jpg" alt = "Logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>   
+                </ul>
+            </div>
+        </div>
+  );
+};
+
+export default Header;
