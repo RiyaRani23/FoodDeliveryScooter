f7d02a89e6f97c2c126fd90c810034357e92f61d
RiyaRani23 <riyarani1404@gmail.com>
2026-01-01 18:42:54 +0530

    Optimising our app

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import {useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body =() => {
     const navigate = useNavigate();
@@ -30,6 +31,15 @@ const Body =() => {
             json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
         );
     };
+
+    const onlineStatus = useOnlineStatus();
+
+    if (onlineStatus === false)
+        return (
+        <h1>
+            Looks like you are offline!! Please check your internet connection
+        </h1>
+        );
 
     return listOfRestaurants.length === 0 ? < Shimmer /> : (
     <div className="body">
