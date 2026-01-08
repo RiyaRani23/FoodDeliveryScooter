cda2db832723c7191f7073aa42d17f39a7a6ad4a
RiyaRani23 <riyarani1404@gmail.com>
2026-01-08 20:37:34 +0530

    testing contact page

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  presets: [
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }]  // Babel Preset => helping to convert jsx into normal html code
+  ],
 };
