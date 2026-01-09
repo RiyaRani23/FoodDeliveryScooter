3145b1ff89aef781917adf5e7274ad52a729ec91
RiyaRani23 <riyarani1404@gmail.com>
2026-01-09 13:33:50 +0530

    deploying

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ const Header = () => {
             <div className="flex items-center">
                 <ul className="flex items-center gap-6 font-medium text-gray-700">
                     <li className="text-sm">
-                        Online Status: { onlineStatus ? "✅ye" : "❌" }
+                        Online Status: { onlineStatus ? "✅" : "❌" }
                     </li>
                     <li className="hover:text-green-600 transition-colors font-bold">
                         <Link to="/">Home</Link>
